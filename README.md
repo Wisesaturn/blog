021ef26db2ccc96dc932be3946f80e77724953c4
Jae Han Song <rfv1479@naver.com>
2022-10-23 05:42:03 +0900

    Chore : Adjust Eslint and Write Stack on README

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - Markdown parsing을 이용하여 자체 블로그를 제작해봅니다.
 - 경험, 프로젝트와 같은 일들을 기록하고자 합니다.
+---
+## Stack
+- TailwindCSS
+- Next.js
+- TypeScript
+- ETC (Eslint, Prettier)
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
-import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   return (
@@ -12,17 +12,15 @@ const Home: NextPage = () => {
 
       <main className="flex w-full flex-1 flex-col items-center justify-center px-20 text-center">
         <h1 className="text-6xl font-bold">
-          Welcome to{" "}
+          Welcome to{' '}
           <a className="text-blue-600" href="https://nextjs.org">
             Next.js!
           </a>
         </h1>
 
         <p className="mt-3 text-2xl">
-          Get started by editing{" "}
-          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">
-            pages/index.tsx
-          </code>
+          Get started by editing{' '}
+          <code className="rounded-md bg-gray-100 p-3 font-mono text-lg">pages/index.tsx</code>
         </p>
 
         <div className="mt-6 flex max-w-4xl flex-wrap items-center justify-around sm:w-full">
@@ -75,8 +73,7 @@ const Home: NextPage = () => {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{" "}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
+          Powered by <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
         </a>
       </footer>
     </div>
