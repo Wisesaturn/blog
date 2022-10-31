f179576fa1b72e4d4bd9a6b93cc9ec1baf85048c
Jae Han Song <rfv1479@gmail.com>
2022-10-31 15:34:25 +0900

    Chore : set and adjust eslint
    - eslint 설정 추가 및 적용
    - tsconfig path 따로 파일로 분류

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  webpack(config) {
+    config.resolve.modules.push(__dirname);
+    return config;
+  },
+};
