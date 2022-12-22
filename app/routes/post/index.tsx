c1c5a0b0ede714bce94a483fad6a2e251a86e435
Jae Han Song <rfv1479@gmail.com>
2022-12-23 00:14:17 +0900

    feat : 기본 CSS 적용

--- app/routes/post/index.tsx
+++ app/routes/post/index.tsx
@@ -1,6 +1,5 @@
-import type { LoaderFunction, LinksFunction } from '@remix-run/node';
+import type { LoaderFunction } from '@remix-run/node';
 import { Link, useLoaderData } from '@remix-run/react';
-import styles from 'highlight.js/styles/github-dark-dimmed.css';
 import * as firstPost from './setting.mdx';
 import * as secondPost from './welcome.mdx';
 
@@ -15,31 +14,20 @@ export const loader: LoaderFunction = () => {
   return [postFromModule(firstPost), postFromModule(secondPost)];
 };
 
-export const links: LinksFunction = () => {
-  return [
-    {
-      rel: 'stylesheet',
-      href: styles,
-    },
-  ];
-};
-
 export const BlogIndex = () => {
   const posts = useLoaderData();
   return (
-    <div className="flex justify-center">
-      <div className="prose lg:prose-xl py-10">
-        <h2>글 목록</h2>
-        <ul>
-          {posts.map((post: any) => (
-            <li key={post.slug}>
-              <Link to={post.slug}>{post.title}</Link>
-              {post.description ? <p className="m-0 lg:m-0">{post.description}</p> : null}
-            </li>
-          ))}
-        </ul>
-      </div>
-    </div>
+    <>
+      <h2>글 목록</h2>
+      <ul>
+        {posts.map((post: any) => (
+          <li key={post.slug}>
+            <Link to={post.slug}>{post.title}</Link>
+            {post.description ? <p className="m-0 lg:m-0">{post.description}</p> : null}
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
