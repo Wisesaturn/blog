4492e85cdc6d95f8a0b4bc94f2671f68d7b51da8
Jae Han Song <rfv1479@gmail.com>
2023-06-07 03:05:15 +0900

    Feat #20 : 로컬 테스트 환경 페이지 구분
    - 로컬에서 테스트할 수 있도록 테스트 페이지 추가

--- app/utils/constant/category.ts
+++ app/utils/constant/category.ts
@@ -3,4 +3,6 @@ export interface CategoryType {
   link: string;
 }
 
-export const CATEGORY_DATA: CategoryType[] = [{ name: '⚙ ETC', link: 'etc' }];
+export const CATEGORY_DATA: CategoryType[] = [{ name: '⚙ ETC', link: 'etc' }].concat(
+  process.env.NODE_ENV === 'development' ? [{ name: '로컬 테스트 환경', link: 'NEED_글작성' }] : [],
+);
