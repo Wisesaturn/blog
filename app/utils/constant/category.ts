df16f1b978308f9515df7a619dd06752fa33b503
Jae Han Song <rfv1479@gmail.com>
2023-07-05 15:30:54 +0900

    hotfix : add the frontend category

--- app/utils/constant/category.ts
+++ app/utils/constant/category.ts
@@ -5,6 +5,7 @@ export interface CategoryType {
 
 export const CATEGORY_DATA: CategoryType[] = [
   { name: '⚙ ETC', link: 'etc' },
+  { name: 'Frontend', link: 'frontend' },
   { name: 'Typescript', link: 'typescript' },
 ].concat(
   process.env.NODE_ENV === 'development' ? [{ name: '로컬 테스트 환경', link: 'NEED_글작성' }] : [],
