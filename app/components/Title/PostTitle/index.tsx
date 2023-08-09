57fe277b217d484cc50a7aeccb7df8ab817fcdbd
Jae Han Song <rfv1479@gmail.com>
2023-08-09 13:43:46 +0900

    hotfix :postTitle font 수정

--- app/components/Title/PostTitle/index.tsx
+++ app/components/Title/PostTitle/index.tsx
@@ -15,19 +15,19 @@ export default function PostTitle(props: PostTitleProps) {
     <section className="relative max-h-60 shadow-lg rounded-xl w-full max-w-layout mx-auto">
         <div className="relative text-white flex h-60 z-10 flex-col items-center justify-center">
           {subTitle && (<span className="bg-white rounded-xl px-4 py-0.5 text-[11px] mb-1 leading-4 md:leading-5 text-black font-bold md:text-[12px]">{subTitle}</span>)}
-          <h2 className="text-[15px] md:text-[2rem] px-4 max-w-[280px] md:max-w-[700px] flex">
+          <h2 className="text-[17px] md:text-[2rem] px-4 max-w-[290px] md:max-w-[700px] flex">
             <span className="text-gray-400">{'<'}</span>
             <span className="px-1 overflow-x-hidden text-ellipsis w-full whitespace-nowrap">
               <span>{mainTitle}</span>
             </span>
             <span className="text-gray-400">{'/>'}</span>
           </h2>
-          <div className="flex items-center text-[0.875rem] justify-center flex-col">
+          <div className="flex items-center md:text-[0.875rem] text-[12px] justify-center flex-col">
             <div className="inline-block align-sub text-gray-300">{createdAt}</div>
             <div className="pt-3 flex flex-wrap items-center justify-center gap-1 md:gap-3 whitespace-nowrap px-4">
               {tags.map((tag: ITags, idx: number) => {
                 return (
-                  <span className="text-gray-400 font-bold md:text-sm text-xs rounded-sm" key={idx}>
+                  <span className="text-gray-400 font-bold rounded-sm" key={idx}>
                     # {tag.name}
                   </span>
                 );
