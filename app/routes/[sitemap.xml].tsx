defdedcdd8f6fe814d836006229fca56d0dd8fb7
Jae Han Song <rfv1479@gmail.com>
2023-08-14 10:35:57 +0900

    modify : sitemap.xml encode 추가

--- app/routes/[sitemap.xml].tsx
+++ app/routes/[sitemap.xml].tsx
@@ -1,37 +1,31 @@
+interface ISitemap {
+  loc : string;
+  lastmod? : string;
+  priority? : string;
+}
+
+const sitePost : ISitemap[] = [
+  {loc : 'https://jaehan.blog', lastmod : '2023-07-05', priority : '1.0'},
+  {loc : 'https://jaehan.blog/typescript/함수-타입-선언하기', lastmod : '2023-06-27'},
+  {loc : 'https://jaehan.blog/frontend/쌩-npm으로-MFE-구축하기-(1)-:-개념', lastmod : '2023-07-05'},
+  {loc : 'https://jaehan.blog/frontend'},
+  {loc : 'https://jaehan.blog/typescript'},
+  {loc : 'https://jaehan.blog/resume'},
+  {loc : 'https://jaehan.blog/all'}
+]
+
 export const loader = () => {
   // handle "GET" request
   // separating xml content from Response to keep clean code.
   const content = `
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>https://jaehan.blog/</loc>
-        <lastmod>2023-07-05</lastmod>
-        <priority>1.0</priority>
-      </url>
-      <url>
-        <loc>https://jaehan.blog/resume</loc>
-        <lastmod>2023-06-20</lastmod>
-      </url>
-      <url>
-        <loc>https://jaehan.blog/typescript/함수-타입-선언하기</loc>
-        <lastmod>2023-06-27</lastmod>
-      </url>
-      <url>
-        <loc>https://jaehan.blog/frontend/쌩-npm으로-MFE-구축하기-(1)-:-개념</loc>
-        <lastmod>2023-07-05</lastmod>
-      </url>
-      <url>
-        <loc>https://jaehan.blog/typescript</loc>
-      </url>
-      <url>
-        <loc>https://jaehan.blog/resume</loc>
-      </url>
-      <url>
-        <loc>https://jaehan.blog/all</loc>
-      </url>
-      <url>
-        <loc>https://jaehan.blog/frontend</loc>
-      </url>
+    ${sitePost.map((item) => (
+      `<url>
+        <loc>${encodeURI(item.loc)}</loc>
+        ${item.lastmod ? (`<lastmod>${item.lastmod}</lastmod>`) : ''}
+        ${item.priority ? (`<priority>${item.priority}</priority>`) : ''}
+      </url>`
+    )).join('')}
     </urlset>
     `;
   // Return the response with the content, a status 200 message, and the appropriate headers for an XML page
