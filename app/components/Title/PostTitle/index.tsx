a4df530726b73bfa955e4f5bd6e3178ce4c6fd8d
Jae Han Song <rfv1479@gmail.com>
2023-09-15 22:10:05 +0900

    feat : 카테고리마다 로고 추가 및 색 변경

--- app/components/Title/PostTitle/index.tsx
+++ app/components/Title/PostTitle/index.tsx
@@ -13,40 +13,44 @@ export default function PostTitle(props: PostTitleProps) {
 
   return (
     <section className="relative max-h-60 shadow-lg rounded-xl w-full max-w-layout mx-auto">
-        <div className="relative text-white flex h-60 z-10 flex-col items-center justify-center">
-          {subTitle && (<span className="bg-white rounded-xl px-4 py-0.5 text-[11px] mb-1 leading-4 text-black font-bold md:text-[12px]">{subTitle}</span>)}
-          <h2 className="text-[17px] md:text-[2rem] px-4 max-w-[290px] md:max-w-[700px] flex">
-            <span className="text-gray-400">{'<'}</span>
-            <span className="px-1 overflow-x-hidden text-ellipsis w-full whitespace-nowrap">
-              <span>{mainTitle}</span>
-            </span>
-            <span className="text-gray-400">{'/>'}</span>
-          </h2>
-          <div className="flex items-center md:text-[0.875rem] text-[12px] justify-center flex-col">
-            <div className="inline-block align-sub text-gray-300">{createdAt}</div>
-            <div className="pt-3 flex flex-wrap items-center justify-center gap-1 md:gap-3 whitespace-nowrap px-4">
-              {tags.map((tag: ITags, idx: number) => {
-                return (
-                  <span className="text-gray-400 font-bold rounded-sm" key={idx}>
-                    # {tag.name}
-                  </span>
-                );
-              })}
-            </div>
+      <div className="relative text-white flex h-60 z-10 flex-col items-center justify-center">
+        {subTitle && (
+          <span className="bg-white rounded-xl px-4 py-0.5 text-[11px] mb-1 leading-4 text-black font-bold md:text-[12px]">
+            {subTitle}
+          </span>
+        )}
+        <h2 className="text-[17px] md:text-[2rem] px-4 max-w-[290px] md:max-w-[700px] flex">
+          <span className="text-gray-400">{'<'}</span>
+          <span className="px-1 overflow-x-hidden text-ellipsis w-full whitespace-nowrap">
+            <span>{mainTitle}</span>
+          </span>
+          <span className="text-gray-400">{'/>'}</span>
+        </h2>
+        <div className="flex items-center md:text-[0.875rem] text-[12px] justify-center flex-col">
+          <div className="inline-block align-sub text-gray-300">{createdAt}</div>
+          <div className="pt-3 flex flex-wrap items-center justify-center gap-1 md:gap-3 whitespace-nowrap px-4">
+            {tags.map((tag: ITags, idx: number) => {
+              return (
+                <span className="text-gray-400 font-bold rounded-sm" key={idx}>
+                  # {tag.name}
+                </span>
+              );
+            })}
           </div>
         </div>
-        <div className="top-0 bottom-0 absolute brightness-[0.25] bg-gray-100 animate-skeletonUI overflow-hidden rounded-xl w-full max-w-layout">
-          <Image
-            loader={remixImageLoader}
-            blurDataURL={thumbnail === '' ? thumbnailDefault : thumbnail}
-            className="rounded-xl w-full h-full object-cover"
-            placeholder="blur"
-            alt="Thumbnail"
-            key={thumbnail === '' ? thumbnailDefault : thumbnail}
-            src={thumbnail === '' ? thumbnailDefault : thumbnail}
-            dprVariants={[1, 3]}
-          />
-        </div>
+      </div>
+      <div className="top-0 bottom-0 absolute brightness-[0.25] bg-gray-100 animate-skeletonUI overflow-hidden rounded-xl w-full max-w-layout">
+        <Image
+          loader={remixImageLoader}
+          blurDataURL={thumbnail === '' ? thumbnailDefault : thumbnail}
+          className="rounded-xl w-full h-full object-cover"
+          placeholder="blur"
+          alt="Thumbnail"
+          key={thumbnail === '' ? thumbnailDefault : thumbnail}
+          src={thumbnail === '' ? thumbnailDefault : thumbnail}
+          dprVariants={[1, 3]}
+        />
+      </div>
     </section>
   );
 }
--- app/components/Title/Title/index.tsx
+++ app/components/Title/Title/index.tsx
@@ -1,3 +1,7 @@
+import { Icon } from '@iconify/react';
+
+import { CATEGORY_DATA } from '@utils/constant/category';
+
 import type { TitleProps } from './types';
 
 /**
@@ -9,10 +13,16 @@ import type { TitleProps } from './types';
 export default function Title(props: TitleProps) {
   const { isContent = 'Seize the day', isSubContent = '' } = props;
 
+  const CategoryIcon = CATEGORY_DATA.filter((item) => item.name === isContent)[0]?.icon;
+
   return (
     <section className="flex flex-col gap-4 items-center justify-center max-w-full mx-auto pt-4 pb-10 md:pt-12 md:pb-20">
-      <h2>
-        <span className="text-gray-200">{'<'}</span> {isContent}{' '}
+      <h2 className="flex items-center justify-center gap-2">
+        <span className="text-gray-200">{'<'}</span>
+        <div className="flex items-center justify-center gap-1">
+          <span>{CategoryIcon && <Icon icon={CategoryIcon} />}</span>
+          <span>{isContent}</span>
+        </div>
         <span className="text-gray-200">{'/>'}</span>
       </h2>
       <p>{isSubContent}</p>
