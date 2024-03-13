c758cc8444b4dee402d7046f6ec2f40e37117737
Jae Han Song <rfv1479@gmail.com>
2024-03-13 19:12:56 +0900

    fix : Responsive mobile margin 추가

--- src/shared/ui/atoms/header/ProgressBar/index.tsx
+++ src/shared/ui/atoms/header/ProgressBar/index.tsx
@@ -19,7 +19,7 @@ export default function ProgressBar() {
   return (
     <>
       <div ref={progressBarRef} className={`z-[10000] fixed h-1 bg-green-dark left-0`} />
-      <div className="w-full z-[9999] fixed h-1 bg-gray-200 left-0" />
+      <div className="w-full z-[9999] fixed h-1 left-0" />
     </>
   );
 }
