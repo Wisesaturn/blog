9b6d9601a43f2ae9c3f903c8a5689088ebbc14a9
Jae Han Song <rfv1479@gmail.com>
2024-03-19 18:55:40 +0900

    fix : Vercel Legacy Redirects 추가

--- src/app/routes/resume/index.tsx
+++
@@ -1,8 +0,0 @@
-import { redirect } from '@remix-run/node';
-
-/**
- * @deprecated /about으로 변경 (redirect)
- * @returns
- */
-// eslint-disable-next-line import/prefer-default-export
-export const loader = () => redirect('/about', 301);
