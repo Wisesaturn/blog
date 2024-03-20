8ad996f484865664fd77bf9adce51f48b6d6c09b
Jae Han Song <rfv1479@gmail.com>
2024-03-21 01:36:25 +0900

    style : category css 수정

--- src/features/post/ui/atoms/CategoryChip/index.tsx
+++ src/features/post/ui/atoms/CategoryChip/index.tsx
@@ -8,8 +8,8 @@ interface CategoryChipProps extends ICategory {
 export default function CategoryChip(props: CategoryChipProps) {
   const { name, selected, handleClick } = props;
 
-  const selectedClass = `${selected ? 'text-white hover:bg-green-darker dark:hover:bg-green-dark bg-green-darker dark:bg-green-dark' : 'text-green-darker dark:text-green-brighter bg-white dark:bg-transparent'}`;
-  const categoryClass = `border-green-darker hover:dark:bg-[#1a1a1a] hover:bg-gray-100 transition-color duration-200 hover:cursor-pointer px-5 py-1.5 border-[1px] rounded-md text-base max-md:text-sm font-light ${selectedClass}`;
+  const selectedClass = `${selected ? 'text-white hover:bg-green-darker dark:hover:bg-green-dark bg-green-darker dark:bg-green-dark' : 'hover:dark:bg-[#1a1a1a] hover:bg-gray-100 text-green-darker dark:text-green-brighter bg-white dark:bg-transparent'}`;
+  const categoryClass = `border-green-darker transition-color duration-200 hover:cursor-pointer px-5 py-1.5 border-[1px] rounded-md text-base max-md:text-sm font-light ${selectedClass}`;
 
   return (
     <button type="button" onClick={handleClick} className={categoryClass}>
