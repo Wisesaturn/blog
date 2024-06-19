a48eee788ad19f283519531239d94b7ad14babc3
Jae Han Song <rfv1479@gmail.com>
2024-06-20 00:36:14 +0900

    fix : gif 파일도 webp로 변환되는 오류 수정

--- src/features/post/api/firebase/uploadImage.ts
+++ src/features/post/api/firebase/uploadImage.ts
@@ -1,8 +1,8 @@
-import { ref, uploadBytes, getDownloadURL } from 'firebase/storage';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 import sharp from 'sharp';
 
-import { storage } from '$shared/middleware/firebase';
 import Logger from '$shared/helper/logger';
+import { storage } from '$shared/middleware/firebase';
 import { IFireStore } from '$shared/types/global';
 
 interface Props extends IFireStore {
@@ -22,17 +22,27 @@ export default async function uploadImage(props: Props): Promise<string> {
     throw WrongSrcError;
   }
 
-  const isSrc = src.replace(/#x26;/g, '&');
+  const formatImageSrc = src.replace(/#x26;/g, '&');
 
-  const response = await fetch(isSrc);
-  const buffer = await response.arrayBuffer();
+  const response = await fetch(formatImageSrc);
+  const rawBuffer = await response.arrayBuffer();
+  // `sharp`로 이미지 파일을 webp로 변환
+  const webpBuffer = await sharp(rawBuffer)
+    .withMetadata()
+    .toFormat('webp', { quality: 100 })
+    .toBuffer();
 
-  // `sharp`로 이미지 파일을 webp buffer로 변환
-  const data = await sharp(buffer).withMetadata().toFormat('webp', { quality: 100 }).toBuffer();
+  // 확장자
+  const ext = formatImageSrc.includes('unsplash')
+    ? String(formatImageSrc.split('fm=').pop()).split('&').shift()
+    : String(formatImageSrc.split('.').pop()).split('?').shift();
+
+  const formatExt = ext === 'gif' ? 'gif' : 'webp';
+  const data = ext === 'gif' ? rawBuffer : webpBuffer;
 
   // 파일 이름
   const filename = decodeURIComponent(
-    String(String(isSrc.split('/').pop()).split('?').shift())
+    String(String(formatImageSrc.split('/').pop()).split('?').shift())
       .split('.')
       .shift()!,
   );
@@ -40,7 +50,7 @@ export default async function uploadImage(props: Props): Promise<string> {
   // 메타데이터
   const metadata = {
     cacheControl: 'public, max-age=31556952, s-maxage=31556952, immutable',
-    contentType: `image/webp`,
+    contentType: `image/${formatExt}`,
   };
 
   // 유닉스 타임
@@ -48,12 +58,12 @@ export default async function uploadImage(props: Props): Promise<string> {
 
   const collectionRef = ref(
     storage,
-    `${collection}/${category}/${title}/${filename}-${hashTime}.webp`,
+    `${collection}/${category}/${title}/${filename}-${hashTime}.${formatExt}`,
   );
 
   // firebase에 올린 파일 주소 얻기
   const imgFirebaseUrl = await uploadBytes(collectionRef, data, metadata).then(async () => {
-    Logger.update(`${filename}-${hashTime}.webp`);
+    Logger.update(`${filename}-${hashTime}.${formatExt}`);
     const url = await getDownloadURL(collectionRef);
     return url;
   });
