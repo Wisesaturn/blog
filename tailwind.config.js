38de9f019a00739c432e2d62ffed5a8c02bb3508
Wisesaturn <rfv1479@gmail.com>
2024-12-12 15:25:45 +0900

    fix : tailwind.confis.js 내용을 esm 형식으로 변환 (node22 기본 esm)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: ['./src/**/*.{js,ts,jsx,tsx}', './.storybook/preview.tsx'],
   darkMode: ['class', '[color-theme="dark"]'],
   theme: {
