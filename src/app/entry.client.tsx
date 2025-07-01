b61be4cf79f34197a9d0f404cb91ccff5fec552e
Wisesaturn <rfv1479@gmail.com>
2025-07-01 17:23:42 +0900

    fix: 불필요한 ServiceWorker Cache 제거

--- src/app/entry.client.tsx
+++ src/app/entry.client.tsx
@@ -17,18 +17,3 @@ startTransition(() => {
     </StrictMode>,
   );
 });
-
-// if the browser supports Service Worker (all modern browsers do it)
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // we will register it after the page complete the load
-    navigator.serviceWorker
-      .register('/service-worker-cache.js')
-      .then((registration) => {
-        console.log('Service Worker registered:', registration.scope);
-      })
-      .catch((error) => {
-        console.error('Service Worker registration failed:', error);
-      });
-  });
-}
