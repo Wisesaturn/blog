eb1c4698c91791c88106d23f3f8f9285d85e1fca
Wisesaturn <rfv1479@gmail.com>
2025-07-04 18:31:42 +0900

    modify: Copyright 지금년도 기준으로 뜨도록 수정

--- src/shared/ui/templates/Layout/Copyright/index.tsx
+++ src/shared/ui/templates/Layout/Copyright/index.tsx
@@ -1,13 +1,16 @@
 import Contact from '$shared/ui/molecules/profile/Contact';
 
 export default function Copyright() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className="text-xs md:text-sm flex justify-center flex-col items-center bg-gray-100 dark:bg-[#191919] mt-10 py-6 gap-1">
       <Contact />
       <span className="text-gray-400 dark:text-gray-200">
         {' '}
-        © 2023-2024 Copyright <span className="text-gray-500 dark:text-white">사툰사툰</span>,
-        based on <span className="text-gray-500 dark:text-white">remix</span>
+        © 2023-{currentYear} Copyright{' '}
+        <span className="text-gray-500 dark:text-white">사툰사툰</span>, based on{' '}
+        <span className="text-gray-500 dark:text-white">remix</span>
       </span>
       <div className="max-md:min-h-[44px] ios-notch-margin-bottom hidden max-md:block" />
     </footer>
